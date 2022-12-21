9e4bd21594a6693e591622fd8653b76fb0ba24d9
Samuel Oliveira <renard162@gmail.com>
2022-12-20 22:22:43 -0300

    Montada função para decodificar a mensagem serial para o trem de pulsos de excitação do sistema.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,52 @@
 #include <Arduino.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <math.h>
 
-int ByteReceived;
-byte inputArray[10000];
-byte outputArray[80000];
+char cmd;
+unsigned char message[2];
+int messageCounter;
+unsigned char signal[32];
+unsigned char samples[256];
 
 void setup() {
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+}
+
+int b8_hex2int(unsigned char *hex){
+   int dec = 0;
+   int x, i;
+   for (i = 1 ; i >= 0 ; --i) {
+      if (hex[i] >= '0' && hex[i] <= '9') {
+         x = hex[i] - '0';
+      }
+      else if (hex[i] >= 'a' && hex[i] <= 'f') {
+         x = hex[i] - 'a' + 10;
+      }
+      dec = dec + x * pow(16 , 1-i);
+   }
+   return dec;
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    ByteReceived = Serial.read();
-    Serial.print(ByteReceived);
-    Serial.println();
+  messageCounter = 1;
+  while(!Serial.available());
+  while (messageCounter >= 0) {
+    cmd = Serial.read();
+    if ((cmd >= '0' && cmd <= '9') || (cmd >= 'a' && cmd <= 'f')) {
+      message[messageCounter] = cmd;
+      messageCounter--;
+    }
   }
+  messageCounter = 1;
+  Serial.print(b8_hex2int(message));
+  Serial.println();
+  // if (cmd == 'l') {
+  //   digitalWrite(13, HIGH); 
+  // }
+  // else if (cmd == 'd') {
+  //   digitalWrite(13, LOW);
+  // }
 }
